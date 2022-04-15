ce1d4a2b9d1baba58d6017ea7c8038f5c2b667bd
PandaGrom <arsenij.grom@mail.ru>
2022-04-15 13:28:21 +0300

    Add style for devise pages

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def index
+    authorize User
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    authorize @user
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new
+  end
+
+  def update
+    @user = User.find(params[:id])
+    authorize User
+    @user.save!
+    redirect_to user_path(@user)
+  end
+end
